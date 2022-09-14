4ddd5ef65c31f914167dee7bf05b7dfb5207cb23
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-09-13 22:45:47 -0400

    update tables

--- backend/vacation-schema.sql
+++ backend/vacation-schema.sql
@@ -12,55 +12,38 @@ CREATE TABLE users (
 
 CREATE TABLE flightReservations (
   id SERIAL PRIMARY KEY,
-  numberOfPassengers NUMERIC NOT NULL,  -- direction STRING,
-  type TEXT NOT NULL, -- note: 'ONE_WAY, ROUND_TRIP'
-  classType TEXT NOT NULL,
+  numberOfPassengers NUMERIC NOT NULL,  
   location_departure TEXT NOT NULL,
   location_arrival TEXT NOT NULL,
-  date_departure DATE NOT NULL,
-  date_arrival DATE NOT NULL,
-  priceMin INTEGER NOT NULL,
-  priceMax INTEGER NOT NULL,
-  sortOrder TEXT NOT NULL
+  departureDate DATE NOT NULL,
+  returnDate DATE NOT NULL,
+  price INTEGER NOT NULL,
+  currency TEXT
 );
 
 CREATE TABLE hotelReservations (
-  id SERIAL PRIMARY KEY,
+  id TEXT PRIMARY KEY,
+  -- id SERIAL PRIMARY KEY,
   hotelName TEXT NOT NULL,
-  checkin DATE NOT NULL,
-  checkout DATE NOT NULL,
-  reviews NUMERIC,
+  roomType TEXT NOT NULL,
+  checkInDate DATE NOT NULL,
+  checkOutDate DATE NOT NULL,
   numberOfGuests NUMERIC,  
   roomsNumber NUMERIC,
-  description TEXT,
   price INTEGER NOT NULL,
-  imgUrl TEXT
+  currency TEXT
 );
 
-CREATE TABLE carRentals (
-  id SERIAL PRIMARY KEY,
-  carName TEXT NOT NULL,
-  location_pickup TEXT NOT NULL,
-  location_return TEXT NOT NULL,
-  date_time_pickup DATE NOT NULL, 
-  date_time_return DATE NOT NULL, 
-  description TEXT,
-  price INTEGER NOT NULL,
-  imgUrl TEXT
-);
 
 CREATE TABLE trips (
   id SERIAL,
-  trip_name TEXT NOT NULL,
-  trip_date DATE NOT NULL,
+  -- trip_name TEXT NOT NULL,
+  -- trip_date DATE NOT NULL,
   username VARCHAR(25)
     REFERENCES users ON DELETE CASCADE,
   flightReservation_id INTEGER
     REFERENCES flightReservations ON DELETE CASCADE,
-  hotelReservation_id INTEGER
+  hotelReservation_id TEXT
     REFERENCES hotelReservations ON DELETE CASCADE,
-  carRental_id INTEGER
-    REFERENCES carRentals ON DELETE CASCADE,
-  PRIMARY KEY (id, username, flightReservation_id, hotelReservation_id, carRental_id)
+    PRIMARY KEY (id, username, flightReservation_id, hotelReservation_id)
 );
-
