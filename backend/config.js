cf37f5ec03d869ff1c7d660884cfe82c88226749
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-09-19 14:33:45 -0400

    update

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,9 @@
 
 /** Shared config for application; can be required many places. */
 
-require("dotenv").config();
+const dotenv = require('dotenv');
+dotenv.config();
+// require("dotenv").config();
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
